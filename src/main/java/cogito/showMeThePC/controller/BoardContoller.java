964317b52a5ef4531f099c8cf10308b8099af860
cogito1016 <cogito1016@naver.com>
2020-07-22 20:45:18 +0900

    feat: 커뮤니티목록 컨트롤러 초안 작성 -컨트롤러 클래스 생성 후 /boards를 사용하여 view단과의 연결까지 확인 *model에 데이터를 넣어 thymeleaf기능을 통해 foreach를 해주어야한다. *reset.css가 왜 깨지는지 파악해야한다

---
+++ src/main/java/cogito/showMeThePC/controller/BoardContoller.java
@@ -0,0 +1,21 @@
+package cogito.showMeThePC.controller;
+
+import cogito.showMeThePC.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardContoller {
+
+    private final BoardService boardService;
+
+    @GetMapping("/boards")
+    public String boards(Model model){
+
+//        model.addAttribute("boards",boardService.findAll());
+        return "index";
+    }
+}
