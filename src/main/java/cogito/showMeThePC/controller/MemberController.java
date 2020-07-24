64d2e19015bc61dc9906d2ba16a3b82141f01612
cogito1016 <cogito1016@naver.com>
2020-07-24 18:55:56 +0900

    feat: MemberDTO & MemberController의 /member(GET,POST) 초안 -MemberDTO를 만들었으니 접근시 해당 객체를 넘겨준다. *넘겨진 객체가 채워진상태로 다시 받아와야함(POST접근시) - 따라서 타임리프의 수정이 필요(문법알아볼것) *MemberDTO -> MemberEntity로 변환시 좀 더 깔끔한 코드를 만들 방법 생각필요!

---
+++ src/main/java/cogito/showMeThePC/controller/MemberController.java
@@ -0,0 +1,29 @@
+package cogito.showMeThePC.controller;
+
+import cogito.showMeThePC.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/member")
+    public String registerForm(Model model){
+        model.addAttribute("form",new MemberDTO()); //폼을 붙여 넘겼으니 뷰에선 그 폼을 채워야함
+        return "registerForm";
+    }//registerForm() end
+
+    @PostMapping("/member")
+    public String register(MemberDTO memberDTO){//1.채워진 폼이 넘겨올테니
+
+        //2.실제 Member엔티티의 객체를 새성해서 폼의 내용을 채워넣고
+        //3.MemberService의 save동작을 수행해야한다.
+        return "index";
+    }
+}
---
+++ src/main/java/cogito/showMeThePC/controller/MemberDTO.java
@@ -0,0 +1,24 @@
+package cogito.showMeThePC.controller;
+
+import cogito.showMeThePC.domain.Address;
+import cogito.showMeThePC.domain.Setting;
+import cogito.showMeThePC.domain.enumType.MemberStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embedded;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+public class MemberDTO {
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+    private String email;
+}
